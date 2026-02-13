eb013b72639d0ece6f40768f637750ea9beb3665
SIMRAN SHARMA <lovelysharma2535@gmail.com>
2026-02-13 13:02:11 +0530

    jh

--- Backend/MiddleWare/checkToken.js
+++ Backend/MiddleWare/checkToken.js
@@ -1,9 +1,20 @@
 
 
-const checkToken = (req,res,next)=>{
-
-
-
+const checkToken = (req, res, next) => {
+    try {
+        const jwtToken = req.headers.authoriztion?.split(" ")[1]
+        if (!jwtToken) {
+            return res.status(401).json({ message: "Unauthorized, Token Missing" });
+        }
+        const verifyToken = jwtToken.verify(jwtToken, "simran")
+        console.log(verifyToken)
+     return verifyToken
+    }
+    catch {
+        (error) => {
+            next(error)
+        }
+    }
 }
 
 model.exports = checkToken
