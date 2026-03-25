790c5485737ce1eee78d7a1594e95ab26ea3977b
kirandeepkaur02 <kirandhiman680@gamil.com>
2026-03-25 10:20:27 +0530

    as

--- Frontend/src/webpage/Index.jsx
+++ Frontend/src/webpage/Index.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import NavBar from './Components/NavBar'
 import HeroSection from './Components/HeroSection'
-
+import ServiceSection from './Components/ServiceSection'
 const index = () => {
   return (
     <div>
          <NavBar />
          <HeroSection />
+
+         
+         <ServiceSection />
     </div>
   )
 }
