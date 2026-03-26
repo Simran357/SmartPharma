896271ebad3628d9cacd27247ef9938a5d479636
Aryan56577 <aryanjasrotia59@gmail.com>
2026-03-26 09:55:53 +0530

    as

--- Frontend/src/webpage/Index.jsx
+++ Frontend/src/webpage/Index.jsx
@@ -4,17 +4,18 @@ import HeroSection from './Components/HeroSection'
 import ServiceSection from './Components/ServiceSection'
 import RolePanel from './Components/RolePanel'
 import AboutSection from './Components/AboutSection'
+import Footer from './Components/Footer'
 
 const index = () => {
   return (
     <div>
-         <NavBar />
-         <HeroSection />
+      <NavBar />
+      <HeroSection />
+      <Services />
+      <RolePanel />
+      <AboutSection />
+      <Footer />
 
-              <RolePanel/>
-              <AboutSection/>
-         <Services/>
-        
     </div>
   )
 }
